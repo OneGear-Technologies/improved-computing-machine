2d021afdeb6b9f7fd2ea2bdfa2328dea188c395c
Anirban Sikdar <anirban9293@gmail.com>
2023-01-01 16:57:33 +0530

    Initial commit

---
+++ Userdemo/user/urls.py
@@ -0,0 +1,12 @@
+from django.urls import path
+from .views import RegisterView, MyObtainTokenPairView
+from rest_framework_simplejwt.views import TokenRefreshView
+from django.contrib.auth import views as auth_views
+from user import views as user_views
+
+urlpatterns = [
+    
+    path('login/', auth_views.LoginView.as_view(template_name='user/login.html'), name='login'),
+    path('logout/', auth_views.LogoutView.as_view(template_name='user/logout.html'), name='logout'),
+    path('register/', user_views.register, name='register'),
+]
