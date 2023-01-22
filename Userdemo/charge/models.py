58865608d07c17be2b81e3b9455d9af31bc0be35
ru2saig <42857541+ru2saig@users.noreply.github.com>
2023-01-23 02:19:34 +0530

    Added uid field to make serializers work right

--- Userdemo/charge/models.py
+++ Userdemo/charge/models.py
@@ -6,6 +6,7 @@ class StatProfile(models.Model):
     #user = models.ForeignKey('auth.User',on_delete = models.SET_NULL,null=True,blank=True)
 
     cid = models.IntegerField(unique=True)
+    uid = models.IntegerField(default=0)
     charge_stat = models.BooleanField(default=False)
 
     def __str__(self) -> str:
